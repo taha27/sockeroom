db21973e249fb058d6872acd9187f5883cb4a4de
taha27 <tahashabbir27@gmail.com>
2017-07-29 22:42:06 -0400

    added a socket io server instance

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const app = require('express')();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello World</h1>');
     res.sendFile(__dirname + '/public/index.html');
-})
-app.listen(3000, () => {
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    });
+});
+
+http.listen(3000, () => {
     console.log('listening on *:3000');
-})
+});
